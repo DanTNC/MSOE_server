76220a1fd539785d7fe20259448474d75f8b0718
lin11220206 <lin11220206@gmail.com>
2017-01-17 20:53:00 +0800

    remove unused script

--- public/js/modechange.js
+++ public/js/modechange.js
@@ -1,11 +1,9 @@
 var edit_mode = () =>{
   $(".left").show();
   $(".panel-group").show();
-  $(".edit_line").show();
 }
 var preview_mode = () =>{
   $(".left").hide();
   $(".panel-group").hide();
-  $(".edit_line").hide();
 }
 
