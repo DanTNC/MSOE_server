51dbccf070061c06d4905d02f3af14b675c5f476
Ting An Yen <fad11204@yahoo.com.tw>
2018-02-26 11:10:20 +0000

    [Security] remove sensitive data from repo

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -5,6 +5,6 @@ var fs = require('fs');
 var data = fs.readFileSync(path.resolve(__dirname, "mongo.json"), 'utf-8');
 var account = JSON.parse(data);
 
-mongoose.connect('mongodb://'+account.id+':'+account.pwd+'@ds147454.mlab.com:47454/'+account.db);
+mongoose.connect('mongodb://'+account.id+':'+process.env.mlab+'@ds147454.mlab.com:47454/'+account.db);
 
 module.exports = mongoose;
