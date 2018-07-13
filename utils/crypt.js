5df23fddebcbc0ffe8046d88a944c69a9e1f977a
Ting An Yen <fad11204@yahoo.com.tw>
2018-07-13 20:09:41 +0000

    [Fix] make randomIndex and randomKey url safe

--- utils/crypt.js
+++ utils/crypt.js
@@ -2,11 +2,17 @@ var crypto = require('crypto');
 
 var crypt = new function () {
     this.randomIndex = function() {
-        return crypto.randomBytes(32).toString('base64').substr(0,10).toUpperCase();
+        return crypto.randomBytes(32).toString('base64').substr(0,10).toUpperCase().replace(/\+/g, '-').replace(/\//g, '_');
     }
     this.randomKey = function() {
-        return crypto.randomBytes(32).toString('base64').substr(0,20).toUpperCase();
+        return crypto.randomBytes(32).toString('base64').substr(0,20).toUpperCase().replace(/\+/g, '-').replace(/\//g, '_');
     }
 };
 
 module.exports = crypt;
+
+if (require.main === module){
+    for(var i = 0;i<10;i++){
+        console.log(crypt.randomIndex());
+    }
+}
