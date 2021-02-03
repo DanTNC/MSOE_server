e515776daca0905da4773d5b87f4a8f30810df93
dantnc <fad11204@yahoo.com.tw>
2021-02-03 01:38:47 +0000

    [Test] align coding style of tests

--- test/add.test.js
+++ test/add.test.js
@@ -1,4 +1,5 @@
-var expect = require('chai').expect;
+const { expect } = require('chai').expect;
+
 describe("Add function", function(){
    it("should return the sum of two variables", function(){
         expect(3 + 4).to.equal(7);
