c47a19e32a9ba4bd6bce7cdfde129b1cbdef8167
dantnc <fad11204@yahoo.com.tw>
2021-02-03 01:37:41 +0000

    [Test] finally selenium test works

--- test/selenium.js
+++ test/selenium.js
@@ -1,15 +1,24 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 const { expect } = require('chai');
 
+const screen = {
+    width: 640,
+    height: 480
+};
+
 describe('DefaultTest', () => {
-    const driver = new Builder().forBrowser('chrome').build();
+    const driver = new Builder()
+                    .forBrowser('chrome')
+                    .setChromeOptions(new chrome.Options().headless().windowSize(screen))
+                    .build();
 
     it('should go to nehalist.io and check the title', async () => {
         await driver.get('https://www.google.com');
-        // await driver.sleep(20000);
         await driver.findElement(By.name('q')).sendKeys('nehalist', Key.ENTER);
         await driver.wait(until.elementLocated(By.id('search')));
-        await driver.findElement(By.linkText('nehalist.io')).click();
+        const link = await driver.findElement(By.xpath("//a[@href='https://nehalist.io/']"));
+        await driver.executeScript('arguments[0].click()', link)
         const title = await driver.getTitle();
 
         expect(title).to.equal('nehalist.io');
