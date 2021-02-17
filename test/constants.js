f5feccfe09cdcad4649b65436ff5892f08a7c3d9
dantnc <fad11204@yahoo.com.tw>
2021-02-17 06:29:17 +0000

    [Refactor] enhance constants.js style

--- test/constants.js
+++ test/constants.js
@@ -1,15 +1,9 @@
-const screen = {
-    width: 1024,
-    height: 768
-};
-
-const PRE_LOADER_TIMEOUT = 5000;
-const ANIMATION_TIMEOUT = 5000;
-const REDIRECT_TIMEOUT = 10000;
-
 module.exports = {
-    screen,
-    PRE_LOADER_TIMEOUT,
-    ANIMATION_TIMEOUT,
-    REDIRECT_TIMEOUT,
+    screen: {
+        width: 1024,
+        height: 768
+    },
+    PRE_LOADER_TIMEOUT: 5000,
+    ANIMATION_TIMEOUT: 5000,
+    REDIRECT_TIMEOUT: 10000,
 };
