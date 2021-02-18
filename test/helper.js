d46839f02cff27f1ca225023db2aed3833cb4bf9
dantnc <fad11204@yahoo.com.tw>
2021-02-18 09:44:04 +0000

    [Refactor] extract element check functions to helper.js

--- test/helper.js
+++ test/helper.js
@@ -12,8 +12,62 @@ function generatePathByIndexKey(editMode, index, key) {
     return '?!' + index + key;
 }
 
+const elementWithStateCheck = async (driver, by, state) => {
+    return (await driver.findElement(by).getAttribute('class')).includes(state);
+};
+
+const elementWithoutStateCheck = async (driver, by, state) => {
+    return !(await elementWithStateCheck(driver, by, state));
+};
+
+const elementWithState = (driver, by, state) => {
+    return async () => {
+        return await elementWithStateCheck(driver, by, state);
+    };
+};
+
+const elementWithoutState = (driver, by, state) => {
+    return async () => {
+        return await elementWithoutStateCheck(driver, by, state);
+    };
+};
+
+const elementAppearsCheck = async (driver, by) => {
+    var appears = true;
+    try {
+        await driver.findElement(by);
+    } catch (NoSuchElementError) {
+        appears = false;
+    }
+    return appears;
+};
+
+const elementDisappearsCheck = async (driver, by) => {
+    return !(await elementApearsCheck(driver, by));
+};
+
+const elementAppears = (driver, by) => {
+    return async () => {
+        return await elementAppearsCheck(driver, by);
+    };
+};
+
+const elementDisappears = (driver, by) => {
+    return async () => {
+        return await elementDisappearsCheck(driver, by);
+    };
+};
+
 module.exports = {
     defaultIndex,
     defaultKey,
     generatePathByIndexKey,
+    elementWithState,
+    elementWithStateCheck,
+    elementWithoutState,
+    elementWithoutStateCheck,
+    elementAppears,
+    elementAppearsCheck,
+    elementDisappears,
+    elementDisappearsCheck,
 };
