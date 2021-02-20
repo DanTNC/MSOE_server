42a4305a26a45904993c02b9dfb869c0167ea33a
dantnc <fad11204@yahoo.com.tw>
2021-02-20 12:44:43 +0000

    [Test] use test database when running tests

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -5,7 +5,9 @@ var fs = require('fs');
 var data = fs.readFileSync(path.resolve(__dirname, "mongo.json"), 'utf-8');
 var account = JSON.parse(data);
 
-// mongoose.connect('mongodb://'+account.id+':'+process.env.mlab+'@ds147454.mlab.com:47454/'+account.db, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect('mongodb+srv://'+account.id+':'+process.env.mlab+'@msoe.iidb2.gcp.mongodb.net/'+account.db+'?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+    'mongodb+srv://'+account.id+':'+process.env.mlab+'@msoe.iidb2.gcp.mongodb.net/msoe?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true, dbName: account.db}
+);
 
 module.exports = mongoose;
