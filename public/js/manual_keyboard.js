dfde4d805f5565137b6fef8c5bf4aae93af3821d
dantnc <fad11204@yahoo.com.tw>
2021-06-12 19:03:14 +0800

    [Feature] make manual keyboard all DOMs

--- public/js/manual_keyboard.js
+++ public/js/manual_keyboard.js
@@ -1,67 +1,175 @@
-$(window).on("load", function(){
-    var explanations = {};
-    $.getJSON("json/key.json", function(res){
-        explanations = res;
-    });
 
-    Key = function(id_, left, width, height){
+var explanations = {};
+var functionKeysConfig = "";
+var keyboardLayout = "";
+var keyContent = "";
+var keyboardKeys = [];
+var functionKeys = [];
+const LAYOUT_TOP = 1.8;
+const LAYOUT_ROW_OFFSET = 11.1;
+
+class Key {
+    constructor(left, width, height) {
+        this.left = Number(left);
+        this.width = Number(width);
+        this.height = Number(height) || 9.8;
+    }
+}
+
+class Group {
+    constructor(top, keys) {
+        this.top = top;
+        this.keys = keys;
+    }
+
+    toDom(parent) {
+        this.keys.forEach((key) => {
+            key.toDom(parent, this.top);
+        });
+    }
+}
+
+class KeyboardKey extends Key {
+    constructor(content, left, width, height) {
+        super(left, width, height);
+        this.content = content;
+    }
+
+    toDom (parent, top) {
+        let key_elem = $("<div class='key keyboardkey'></div>");
+        key_elem.css("left", `calc(${this.left}vh + (100vw - 163.5vh) / 2)`);
+        key_elem.css("width", `${this.width}vh`);
+        key_elem.css("height", `${this.height}vh`);
+        key_elem.css("line-height", `${this.height}vh`);
+        key_elem.css("top", `${top}vh`);
+        key_elem.html("<p>" + this.content + "</p>");
+        parent.append(key_elem);
+    }
+
+}
+class FunctionKey extends Key {
+    constructor(id_, left, width, height) {
+        super(left, width, height);
         this.id_ = id_;
-        this.left = left;
-        this.width = width;
-        this.height = height || 9.8;
-    };
+    }
 
-    var generateKeys = (keys) => {
-        var top = 1.8;
-    
-        for (row of keys) {
-            for (key of row) {
-                let key_elem = $("<div class='key' data-value='" + key.id_ + "'></div>");
-                key_elem.css("left", (key.left - 24.56)*$(window).height()/100+ $("#keyboard .ui.image").offset().left);
-                key_elem.css("width", key.width*$(window).height()/100);
-                key_elem.css("height", key.height*$(window).height()/100);
-                key_elem.css("top", top*$(window).height()/100);
-                $("#keys").append(key_elem);
-            }
-            top += 11.1;
-        }
+    toDom (parent, top) {
+        let key_elem = $("<div class='key functionkey' data-value='" + this.id_ + "'></div>");
+        key_elem.css("left", `calc(${this.left}vh + (100vw - 163.5vh) / 2)`);
+        key_elem.css("width", `${this.width}vh`);
+        key_elem.css("height", `${this.height}vh`);
+        key_elem.css("top", `${top}vh`);
+        parent.append(key_elem);
+    }
+}
 
-        console.log("Regenerate keys.");
-        console.log($(window).height());
-    };
+var generateKeys = () => {
+    generateKeyboardKeys();
+    generateFunctionKeys();
 
-    var keys = [ //[r, k, (left, width, height)]
-        [new Key("1", 159.5, 27.5)],
-        [new Key("I", 37, 65.5), new Key("J", 149, 9.8), new Key("K", 160.2, 9.8), new Key("L", 171.4, 15.8)],
-        [
-            new Key("8", 42.6, 9.8), new Key("9", 53.8, 9.8), new Key("A", 65, 9.8),
-            new Key("B", 76.2, 9.8), new Key("C", 87.4, 9.8), new Key("D", 98.6, 9.8),
-            new Key("E", 143.6, 9.8), new Key("F", 154.8, 9.8), new Key("G", 166, 9.8), new Key("H", 177.2, 9.8)
-        ],
-        [
-            new Key("3", 44.8, 9.8), new Key("4", 56, 9.8), new Key("5", 67.2, 9.8),
-            new Key("6", 134.6, 9.8), new Key("7", 157, 9.8), new Key("P", 168.2, 18.8)
-        ],
-        [
-            new Key("Q", 25.6, 23.4), new Key("2", 50.4, 77.3), new Key("M", 129, 9.8), 
-            new Key("N", 140.2, 9.8), new Key("O", 151.4, 9.8), new Key("Q", 162.6, 24.6)
-        ],
-        [
-            new Key("R", 25.6, 12.2, 12), new Key("S", 50.4, 9.8, 12), new Key("S", 129, 9.8, 12), 
-            new Key("R", 140.2, 9.8, 12), new Key("1", 151.6, 35.6, 12)
-        ]
-    ];
-
-    generateKeys(keys);
-
-    $(window).resize(function(){
-        $(".key").remove();
-        generateKeys(keys);
-    });
-
-    $(".key").hover(function(){
+    $(".functionkey").hover(function(){
         $("#explanation").html("<div class='line'>" + explanations[$(this).attr("data-value")].split("<br>").join("</div><div class='line'>") + "</div>");
     }, function(){
         $("#explanation").html("<div class='line'>Point on a key or a key group to see the command of it.</div>");
     });
+}
+
+var generateKeyboardKeys = () => {
+    let parent = $("#keyboard-keys");
+
+    for (group of keyboardKeys) {
+        group.toDom(parent);
+    }
+}
+
+var generateFunctionKeys = () => {
+    let top = LAYOUT_TOP;
+    let parent = $("#function-keys");
+
+    for (row of functionKeys) {
+        for (key of row) {
+            key.toDom(parent, top);
+        }
+        top += LAYOUT_ROW_OFFSET;
+    }
+};
+
+var initializeKeyboardKeys = () => {
+    const defConfigs = {gap: 1, left: 0, height: 1};
+    var defaultSession = true;
+    var top = LAYOUT_TOP;
+
+    var contentGroups = keyContent.split(/\r\n|\n/).reverse();
+
+    for (let line of keyboardLayout.split(/\r\n|\n/)) {
+        line = line.replace(/\s+/g, "");
+        if (line.startsWith("!")) {
+            if (!defaultSession) {
+                console.warn("Cannot modify default setting after starting defining layout.");
+                continue;
+            }
+            line = line.substring(1);
+            let [param, val] = line.split("=");
+            defConfigs[param] = Number(val);
+        } else {
+            if (line == "") continue;
+            let nextTopOffset = LAYOUT_ROW_OFFSET;
+            if (line.includes("+")){
+                [line, nextTopOffset] = line.split("+");
+                nextTopOffset = Number(nextTopOffset);
+            }
+            for (let group of line.split(";")) {
+                if (group == "") continue;
+                let keyContents = contentGroups.pop().split(" ").reverse();
+                let configs = {};
+                let keys = [];
+                Object.assign(configs, defConfigs);
+                let [configOverrides, layout] = group.split("=");
+                let [gap, left, height] = configOverrides.split(",");
+                if (gap != "%") configs.gap = Number(gap);
+                if (left != "%") configs.left = Number(left);
+                if (height != "%") configs.height = Number(height);
+                for (let keyCombo of layout.split(",")) {
+                    let [times, width] = keyCombo.split(":");
+                    width = Number(width);
+                    for (let i = 0; i < times; i++) {
+                        let keyContent = keyContents.pop().replace(/\//g, "<br/>");
+                        keys.push(new KeyboardKey(keyContent, configs.left, width, configs.height));
+                        configs.left += configs.gap + width;
+                    }
+                }
+                keyboardKeys.push(new Group(top, keys));
+            }
+            top += nextTopOffset;
+        }
+    }
+};
+
+var initializeFunctionKeys = () => {
+    for (let line of functionKeysConfig.split(/\r\n|\n/)) {
+        let row = [];
+        for (let key of line.split("; ")) {
+            if (key == "") continue;
+            let args = key.split(" ");
+            row.push(new FunctionKey(...args));
+        }
+        if (row.length == 0) continue;
+        functionKeys.push(row);
+    }
+};
+
+var initialize = async () => {
+    functionKeysConfig = await $.get("keyboard/functionkey.txt");
+    keyboardLayout = await $.get("keyboard/keyboard.txt");
+    keyContent = await $.get("keyboard/keycontent.txt");
+    explanations = await $.getJSON("json/key.json");
+
+    initializeKeyboardKeys();
+    initializeFunctionKeys();
+    
+    generateKeys();
+};
+
+$(window).on("load", function(){
+    initialize();
 });
